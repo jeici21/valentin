511ae6027b425a9a47b5256fe8efc1f1e464f353
Jorge Castro <david92_jc@yahoo.es>
2024-02-07 17:37:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ¿Quieres ser mi Valentín?
+
+Todos lo estaban haciendo, así que no me quería quedar atrás XD.
---
+++ arrays.js
@@ -0,0 +1,31 @@
+const frases = [
+  "¿Segura que no? 😨",
+  "¿Totalmente segura? 🤔",
+  "¿Pero segura segura? 👀",
+  "¿Positivamente segura que no? 😨",
+  "Seguramente te equivocaste al dar clic 😅",
+  "¿No te están obligando a negarte? 😕",
+  "Estaré muy triste 😢",
+  "Estaré muy muy triste 😔",
+  "Pero muy muy muy triste 😭",
+  "Estaré supermegahiperrecontra triste 🤧",
+  "Me rompes el corazón 🥺",
+  "Vamos, dí que sí 😏",
+  "¡Di que sí! 😙",
+  "Anda, ¡dí que siiiiii! 🥲",
+  "Por favor 🙏",
+  "Por favoooooooooor 🥺",
+  "Piénsalo bien... 🤔",
+  "Sé que quieres decir sí 😍",
+  "Está bien, dejaré de preguntar 😒",
+  "Es broma, dí que sí por favor 🙏"
+];
+
+const gifsTristes = [
+  "https://media.tenor.com/925LDfyVUGEAAAAi/cute-sad.gif",
+  "https://media.tenor.com/wqYFElBnfoYAAAAi/yelynn-yelynnn.gif",
+  "https://media.tenor.com/KkDb5-sgVZkAAAAi/sad-anxious.gif",
+  "https://media.tenor.com/YoAqt_jAr-UAAAAi/sad-broken-heart.gif",
+  "https://media.tenor.com/fYlDBJAaG3AAAAAi/tkthao219-bubududu.gif",
+  "https://media.tenor.com/FPzGMNfFGnYAAAAi/cute-cat.gif"
+]
