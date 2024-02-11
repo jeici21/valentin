09445265ea4815a7983d5ac399f16ea9880404c9
Jorge Castro <david92_jc@yahoo.es>
2024-02-11 14:57:02 -0500

    correcciones menores

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(() => {
   let noClicks = 0, i = 0;
   $('#btn-si').on('click', () => {
-    $('body').html(`
+    $('body').html(/*html*/`
     <img src="https://media.tenor.com/NsouEvrISgQAAAAi/love.gif" alt="Gif" width="200" height="200">
     <h1 class="animate">¡AAAAAAH MUERO DE AMOR!</h1>
   `)
   })
   $('#btn-no').on('click', () => {
-    $('#gif').html(`<img src=${gifsTristes[i]} alt="Gif" width="200" height="200">`)
+    $('#gif').html(/*html*/`<img src=${gifsTristes[i]} alt="Gif" width="200" height="200">`)
     i++;
     if (i >= gifsTristes.length) i = 0;
     $("#btn-no").text(frases[noClicks]);
